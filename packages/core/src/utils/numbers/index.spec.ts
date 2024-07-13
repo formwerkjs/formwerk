5cd7df38e813fe5783396019c70a820b71ff8fd3
Abdelrahman Awad <logaretm1@gmail.com>
2024-07-14 01:15:54 +0300

    feat: handle different numbering systems

--- packages/core/src/utils/numbers/index.spec.ts
+++ packages/core/src/utils/numbers/index.spec.ts
@@ -4,43 +4,44 @@ const enNumber = 1234567890.12;
 
 describe('useNumberParser', () => {
   test('parses localized numbers', () => {
-    const { parse } = useNumberParser({ locale: 'ar-EG' });
+    const { parse, isValidNumber } = useNumberParser('ar-EG');
 
     expect(parse('١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢')).toBe(enNumber);
+    expect(isValidNumber('١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢')).toBe(true);
   });
 
   test('formats localized numbers', () => {
-    const { format } = useNumberParser({ locale: 'ar-EG' });
+    const { format } = useNumberParser('ar-EG');
 
     expect(format(enNumber)).toBe('١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢');
   });
 
   test('parses localized values with negative sign', () => {
-    const { parse } = useNumberParser({ locale: 'ar-EG' });
+    const { parse, isValidNumber } = useNumberParser('ar-EG');
 
     expect(parse('-١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢')).toBe(-enNumber);
+    expect(isValidNumber('-١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢')).toBe(true);
   });
 
   test('formats localized values with negative sign', () => {
-    const { format } = useNumberParser({ locale: 'ar-EG' });
+    const { format } = useNumberParser('ar-EG');
     const formatted = format(-enNumber);
 
     expect(formatted).toBe('-١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢');
   });
 
   test('parses localized currency values', () => {
-    const { parse } = useNumberParser({
-      locale: 'ar-EG',
+    const { parse, isValidNumber } = useNumberParser('ar-EG', {
       style: 'currency',
       currency: 'EGP',
     });
 
     expect(parse('١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢ ج.م.')).toBe(enNumber);
+    expect(isValidNumber('١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢ ج.م.')).toBe(true);
   });
 
   test('formats localized currency values', () => {
-    const { format } = useNumberParser({
-      locale: 'ar-EG',
+    const { format } = useNumberParser('ar-EG', {
       style: 'currency',
       currency: 'EGP',
     });
@@ -49,18 +50,17 @@ describe('useNumberParser', () => {
   });
 
   test('parses negative localized currency values', () => {
-    const { parse } = useNumberParser({
-      locale: 'ar-EG',
+    const { parse, isValidNumber } = useNumberParser('ar-EG', {
       style: 'currency',
       currency: 'EGP',
     });
 
     expect(parse('-١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢ ج.م.')).toBe(-enNumber);
+    expect(isValidNumber('-١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢ ج.م.')).toBe(true);
   });
 
   test('formats negative localized currency values', () => {
-    const { format } = useNumberParser({
-      locale: 'ar-EG',
+    const { format } = useNumberParser('ar-EG', {
       style: 'currency',
       currency: 'EGP',
     });
@@ -69,24 +69,32 @@ describe('useNumberParser', () => {
   });
 
   test('parses han numbers', () => {
-    const { parse } = useNumberParser({ locale: 'zh-Hans-CN-u-nu-hanidec' });
+    const { parse, isValidNumber } = useNumberParser('zh-Hans-CN-u-nu-hanidec');
 
     expect(parse('一二三,四五六.七八九')).toBe(123456.789);
+    expect(isValidNumber('一二三,四五六.七八九')).toBe(true);
   });
 
   test('formats to numbers', () => {
-    const { format } = useNumberParser({ locale: 'zh-Hans-CN-u-nu-hanidec' });
+    const { format } = useNumberParser('zh-Hans-CN-u-nu-hanidec');
 
     expect(format(123456.789)).toBe('一二三,四五六.七八九');
   });
 
   test('parsing/formatting is consistent', () => {
-    const { format, parse } = useNumberParser({ locale: 'ar-EG', style: 'currency', currency: 'EGP' });
+    const { format, parse } = useNumberParser('ar-EG', { style: 'currency', currency: 'EGP' });
 
     const value = 1234567.89;
     const formatted = format(value);
     const parsed = parse(formatted);
 
     expect(parsed).toBe(value);
+  });
+
+  test('tries different numbering systems for parsing', () => {
+    const { parse, isValidNumber } = useNumberParser('en-US', { style: 'currency', currency: 'USD' });
+
+    expect(parse('$ ١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢')).toBe(enNumber);
+    expect(isValidNumber('$ ١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢')).toBe(true);
   });
 });
--- packages/core/src/utils/numbers/index.ts
+++ packages/core/src/utils/numbers/index.ts
@@ -1,9 +1,14 @@
-import { computed, MaybeRefOrGetter, toValue } from 'vue';
+import { MaybeRefOrGetter, toValue } from 'vue';
 
 const SYMBOL_PART_TYPES: Partial<Record<Intl.NumberFormatPartTypes, boolean>> = {
   percentSign: true,
   currency: true,
 };
+
+/**
+ * Numbering systems that are supported by Intl.NumberFormat, user input may not match their own locale/site accepted locale.
+ */
+const NUMBERING_SYSTEMS = ['latn', 'arab', 'hanidec'];
 
 /**
  * Zero widths and RTL and LTR markers are produced sometimes with Intl.NumberFormat, we need to remove them to get as clean as a number as possible.
@@ -12,51 +17,200 @@ const NON_PRINTABLE_RE = /\p{C}/gu;
 
 const SPACES_RE = /\s/g;
 
-export function useNumberParser(locale: MaybeRefOrGetter<string>, opts?: MaybeRefOrGetter<Intl.NumberFormatOptions>) {
-  const formatter = computed(() => new Intl.NumberFormat(toValue(locale), toValue(opts)));
+interface NumberSymbols {
+  /**
+   * The decimal separator.
+   */
+  decimal?: string;
+  /**
+   * The thousands separator.
+   */
+  group?: string;
+  /**
+   * Currency symbol or percent sign or any unit.
+   */
+  literals?: string;
 
-  const parts = computed(() => formatter.value.formatToParts(12345.6789));
+  /**
+   * A regular expression to match numerals in the current locale.
+   */
+  numeralRE: RegExp;
 
-  const getDecimal = () => parts.value.find(part => part.type === 'decimal')?.value || '.';
-  const getGroup = () => parts.value.find(part => part.type === 'group')?.value || ',';
-  const getUnitSymbol = () => parts.value.find(part => SYMBOL_PART_TYPES[part.type])?.value;
+  minusSign?: string;
 
-  const groupRE = computed(() => new RegExp(`[${getGroup()}]`, 'g'));
-  const decimalRE = computed(() => new RegExp(`[${getDecimal()}]`));
-  const symbolRE = computed(() => {
-    const symbol = getUnitSymbol();
+  plusSign?: string;
 
-    return symbol ? new RegExp(`${getUnitSymbol()}`) : null;
-  });
+  /**
+   * Converts a locale numeral to a number (latin).
+   */
+  resolveNumber: (number: string) => string;
+}
 
-  const numerals = computed(() =>
-    [...new Intl.NumberFormat(toValue(locale), { useGrouping: false }).format(9876543210)].reverse(),
-  );
-  const getNumberingMap = () => new Map(numerals.value.map((d, i) => [d, i]));
-  const numeralRE = computed(() => new RegExp(`[${numerals.value.join('')}]`, 'g'));
+interface NumberParser {
+  formatter: Intl.NumberFormat;
+  options: Intl.ResolvedNumberFormatOptions;
+  locale: string;
+  symbols: NumberSymbols;
+  parse(value: string): number;
+  format(value: number): string;
+  isValidNumber(value: string): boolean;
+}
 
-  function resolveNumber(number: string) {
-    return String(getNumberingMap().get(number)) || '';
+const numberParserCache = new Map<string, NumberParser>();
+
+function getParser(locale: string, options: Intl.NumberFormatOptions) {
+  const cacheKey = locale + JSON.stringify(options);
+  let parser = numberParserCache.get(cacheKey);
+  if (!parser) {
+    parser = defineNumberParser(locale, options);
+    numberParserCache.set(cacheKey, parser);
+  }
+
+  return parser;
+}
+
+export function defineNumberParser(locale: string, options: Intl.NumberFormatOptions): NumberParser {
+  const formatter = new Intl.NumberFormat(locale, options);
+  const parts = formatter.formatToParts(-12345.6789);
+  const decimal = parts.find(part => part.type === 'decimal')?.value || '.';
+  const group = parts.find(part => part.type === 'group')?.value || ',';
+  const literals = parts.find(part => SYMBOL_PART_TYPES[part.type])?.value;
+  const minusSign = parts.find(part => part.type === 'minusSign')?.value;
+  const plusSign = parts.find(part => part.type === 'plusSign')?.value;
+  const numerals = [...new Intl.NumberFormat(toValue(locale), { useGrouping: false }).format(9876543210)].reverse();
+
+  const numeralMap = new Map(numerals.map((d, i) => [d, i]));
+  const numeralRE = new RegExp(`[${numerals.join('')}]`, 'g');
+
+  const symbols: NumberSymbols = {
+    decimal,
+    group,
+    literals: literals,
+    numeralRE,
+    minusSign,
+    plusSign,
+    resolveNumber: (number: string) => {
+      return String(numeralMap.get(number)) || '';
+    },
+  };
+
+  function sanitize(value: string): string {
+    let sanitized = value;
+    if (symbols.group) {
+      sanitized = sanitized.replaceAll(symbols.group, '');
+    }
+
+    if (symbols.decimal) {
+      sanitized = sanitized.replace(symbols.decimal, '.');
+    }
+
+    if (symbols.literals) {
+      sanitized = sanitized.replace(symbols.literals, '');
+    }
+
+    sanitized = sanitized.replace(symbols.numeralRE, symbols.resolveNumber).replace(SPACES_RE, '');
+
+    return sanitized.trim();
   }
 
   function parse(value: string): number {
-    let parsed = value.replace(groupRE.value, '').replace(decimalRE.value, '.').replace(numeralRE.value, resolveNumber);
-    if (symbolRE.value) {
-      parsed = parsed.replace(symbolRE.value, '');
-    }
+    const parsed = sanitize(value);
 
-    parsed = parsed.replace(SPACES_RE, '');
-
-    return Number(parsed.trim());
+    return Number(parsed);
   }
 
   function format(value: number): string {
-    return formatter.value.format(value).replace(NON_PRINTABLE_RE, '').trim();
+    return formatter.format(value).replace(NON_PRINTABLE_RE, '').trim();
+  }
+
+  function isValidNumber(value: string) {
+    let sanitized = value;
+    if (symbols.group) {
+      sanitized = sanitized.replaceAll(symbols.group, '');
+    }
+
+    if (symbols.decimal) {
+      sanitized = sanitized.replace(symbols.decimal, '');
+    }
+
+    if (symbols.literals) {
+      sanitized = sanitized.replace(symbols.literals, '');
+    }
+
+    if (symbols.minusSign) {
+      sanitized = sanitized.replace(symbols.minusSign, '');
+    }
+
+    if (symbols.plusSign) {
+      sanitized = sanitized.replace(symbols.plusSign, '');
+    }
+
+    sanitized = sanitized.replace(symbols.numeralRE, '').replace(SPACES_RE, '');
+
+    return sanitized.length === 0;
+  }
+
+  return {
+    formatter,
+    locale,
+    options: formatter.resolvedOptions(),
+    symbols,
+    parse,
+    format,
+    isValidNumber,
+  };
+}
+
+export function useNumberParser(locale: MaybeRefOrGetter<string>, opts?: MaybeRefOrGetter<Intl.NumberFormatOptions>) {
+  function resolveParser(value: string) {
+    const defaultLocale = toValue(locale);
+    const defaultOpts = toValue(opts) || {};
+    // Gets the default parser as per the user config
+    const defaultParser = getParser(defaultLocale, defaultOpts);
+
+    // If the value is a valid number, return the default parser as it is good enough to parse it.
+    // Or if the locale has a hardcoded numbering system, we quit and return the default one.
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale#description
+    if (defaultParser.isValidNumber(value) || defaultLocale.includes('-nu-')) {
+      return defaultParser;
+    }
+
+    // Otherwise, we need to find the correct parser for the value, this is because the value may be in a different numbering system.
+    // The user may prefer to use a different numbering system than the one that is used in the locale.
+    for (const numberingSystem of NUMBERING_SYSTEMS) {
+      // Skip loop if the default numbering system is the same as the one we are trying.
+      if (defaultParser.options.numberingSystem === numberingSystem) {
+        continue;
+      }
+
+      const tryLocale = defaultLocale.includes('-u-')
+        ? `${defaultLocale}-nu-${numberingSystem}`
+        : `${defaultLocale}-u-nu-${numberingSystem}`;
+
+      const parser = getParser(tryLocale, defaultOpts);
+      if (parser.isValidNumber(value)) {
+        return parser;
+      }
+    }
+
+    return defaultParser;
+  }
+
+  function parse(value: string): number {
+    return resolveParser(value).parse(value);
+  }
+
+  function isValidNumber(value: string) {
+    return resolveParser(value).isValidNumber(value);
+  }
+
+  function format(value: number): string {
+    return getParser(toValue(locale), toValue(opts) || {}).format(value);
   }
 
   return {
     parse,
     format,
-    formatToParts: (value: number) => formatter.value.formatToParts(value),
+    isValidNumber,
   };
 }
