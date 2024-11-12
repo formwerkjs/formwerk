affc6af70704bdbcb5780a200251b20cfeb849c6
Abdelrahman Awad <logaretm1@gmail.com>
2024-11-13 00:08:46 +0200

    feat: implement cascading disabled context for all inputs

--- packages/core/src/helpers/createDisabledContext/createDisabledContext.ts
+++ packages/core/src/helpers/createDisabledContext/createDisabledContext.ts
@@ -1,5 +1,4 @@
-import { InjectionKey, Ref, computed, inject, provide, toValue } from 'vue';
-import { Getter } from '../../types';
+import { InjectionKey, MaybeRefOrGetter, Ref, computed, inject, provide, toValue } from 'vue';
 
 interface DisabledContext {
   isDisabled: Ref<boolean>;
@@ -7,7 +6,13 @@ interface DisabledContext {
 
 const DisabledContextKey: InjectionKey<DisabledContext> = Symbol('disabledContextKey');
 
-export function createDisabledContext(isDisabled?: Ref<boolean | undefined> | Getter<boolean | undefined>) {
+/**
+ * Create a disabled context.
+ * @param isDisabled - The disabled state.
+ * @param terminate - Whether to terminate the context and not provide it further.
+ * @returns The disabled state.
+ */
+export function createDisabledContext(isDisabled?: MaybeRefOrGetter<boolean | undefined>) {
   const parentContext = inject(DisabledContextKey, null);
   const context: DisabledContext = {
     isDisabled: computed(() => parentContext?.isDisabled.value || toValue(isDisabled) || false),
