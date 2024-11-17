a74dcc0662e80ad61ad3e58ec08247d3a6513db4
Abdelrahman Awad <logaretm1@gmail.com>
2024-11-17 21:02:18 +0200

    fix: add some tolerance around small steps

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -13,6 +13,7 @@ import InputSelect from './components/InputSelect.vue';
 import OptionGroup from './components/OptionGroup.vue';
 import OptionItem from './components/OptionItem.vue';
 import FormGroup from './components/FormGroup.vue';
+import { ref } from 'vue';
 
 const { handleSubmit, values } = useForm<
   FormSchema<{
@@ -24,8 +25,10 @@ const { handleSubmit, values } = useForm<
   }>
 >();
 
+const value = ref('One');
 const onSubmit = handleSubmit(data => {
   console.log(data.toObject());
+  value.value = 'Two';
 });
 
 type Step = 'One' | 'Two' | 'Three';
@@ -37,6 +40,10 @@ const stops: Step[] = ['One', 'Two', 'Three'];
   <form class="flex flex-col gap-4 w-full" novalidate @submit="onSubmit">
     <h2 class="text-2xl font-bold text-white">Registration Form</h2>
 
-    <Slider :stops="stops" />
+    <Slider v-model="value" :stops="stops" />
+
+    {{ value }}
+
+    <button>Submit</button>
   </form>
 </template>
