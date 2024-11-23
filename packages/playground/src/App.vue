a7c820a622210f91ad2c8a85b8986ab66a04502b
Abdelrahman Awad <logaretm1@gmail.com>
2024-11-23 05:09:45 +0200

    test(slider): added tests for the options prop

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -2,7 +2,7 @@
 import { FormSchema, useForm } from '@formwerk/core';
 import InputText from './components/InputText.vue';
 import Switch from './components/Switch.vue';
-import Slider from './components/Slider.vue';
+import MultiSlider from './components/MultiSlider.vue';
 import Radio from './components/RadioItem.vue';
 import RadioGroup from './components/RadioGroup.vue';
 import CheckboxGroup from './components/CheckboxGroup.vue';
@@ -15,21 +15,7 @@ import OptionItem from './components/OptionItem.vue';
 import FormGroup from './components/FormGroup.vue';
 import { ref } from 'vue';
 
-const { handleSubmit, values } = useForm<
-  FormSchema<{
-    name: string;
-    email: string;
-    subscribe: boolean;
-    plan: string;
-    preferences: string[];
-  }>
->();
-
-const value = ref('One');
-const onSubmit = handleSubmit(data => {
-  console.log(data.toObject());
-  value.value = 'Two';
-});
+const value = ref(['One', 'Three']);
 
 type Step = 'One' | 'Two' | 'Three';
 
@@ -37,13 +23,10 @@ const stops: Step[] = ['One', 'Two', 'Three'];
 </script>
 
 <template>
-  <form class="flex flex-col gap-4 w-full" novalidate @submit="onSubmit">
-    <h2 class="text-2xl font-bold text-white">Registration Form</h2>
+  <h2 class="text-2xl font-bold text-white">Registration Form</h2>
 
-    <Slider v-model="value" :stops="stops" />
+  <MultiSlider v-model="value" :options="stops" />
 
-    {{ value }}
-xx
-    <button>Submit</button>
-  </form>
+  {{ value }}
+  <button>Submit</button>
 </template>
