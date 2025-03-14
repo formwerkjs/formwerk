8cc6fe7c1c2e370617a91926c68ccecf1336d8aa
Eugen Istoc <eugenistoc@gmail.com>
2025-03-13 21:57:05 -0400

    chore: add reset playground

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
-import OtpField from '@/components/OtpField.vue';
+import z from 'zod';
+import DateField from '@/components/DateField.vue';
+import Calendar from '@/components/Calendar.vue';
+import InputText from '@/components/InputText.vue';
+import FormGroup from '@/components/FormGroup.vue';
+import { createCalendar, IslamicUmalquraCalendar } from '@internationalized/date';
 import { useForm } from '@formwerk/core';
-const form = useForm();
+const calendar = createCalendar('islamic-umalqura');
 
-function onCompleted(value: string) {
-  alert(`onCompleted: ${value}`);
-}
+// You need to be careful with the time component of the date object.
+// JS date objects fills the date time with the current time component.
+
+const schema = z.object({
+  company: z.string(),
+  employee: z.object({
+    name: z.string().nonempty(),
+    email: z.string().email(),
+  }),
+});
+const { values, ...form } = useForm({
+  schema,
+});
+
+const reset = () => {
+  form.reset('company', { values: { company: 'Reset Company' } });
+};
 </script>
 
 <template>
-  <OtpField name="otp" label="OTP" />
-
-  <div class="flex w-full">
-    <button @click="form.setValue('otp', 'G-4321')">Set OTP</button>
+  <div class="flex flex-col gap-4">
+    <pre>{{ values }}</pre>
+    <button @click="reset">Reset</button>
+    <InputText label="Company" name="company" />
+    <FormGroup label="Employee" name="employee">
+      <InputText label="Name" name="name" />
+      <InputText label="Email" name="email" />
+    </FormGroup>
   </div>
 </template>
