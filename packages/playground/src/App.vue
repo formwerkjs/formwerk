a88aee07b5a0620c164f8e8d64899c5f46526e5b
Abdelrahman Awad <me@awad.dev>
2025-04-03 01:29:46 -0400

    feat: initial draft

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,27 +1,38 @@
 <script setup lang="ts">
-import { FileUploadContext, useForm } from '@formwerk/core';
-import TimeInput from './components/TimeField.vue';
-import Dropzone from './components/Dropzone.vue';
+import { useFormWizard, FormWizardStep } from '@formwerk/core';
+import InputText from './components/InputText.vue';
+import Switch from './components/Switch.vue';
 
-const form = useForm();
-
-function handleUpload({ file, signal }: FileUploadContext) {
-  return new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('https://example.com/file.png');
-    }, 1000);
-  });
-}
-
-const formatOptions = {
-  hour: '2-digit',
-  minute: '2-digit',
-  second: '2-digit',
-};
+const { wizardProps, nextButtonProps, previousButtonProps } = useFormWizard();
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col items-center justify-center`">
-    <TimeInput label="Time" name="time" :format-options="formatOptions" />
+  <div v-bind="wizardProps" class="w-full h-full flex flex-col items-center justify-center`">
+    <div>
+      <h1>Form Wizard</h1>
+    </div>
+
+    <FormWizardStep>
+      <div>
+        <h2>Step 1</h2>
+      </div>
+
+      <InputText name="name" label="Name" />
+      <InputText name="email" label="Email" />
+    </FormWizardStep>
+
+    <FormWizardStep>
+      <div>
+        <h2>Step 2</h2>
+      </div>
+
+      <InputText name="address" label="Address" />
+      <Switch name="terms" label="I accept the terms and conditions" />
+    </FormWizardStep>
+
+    <div class="grid grid-cols-2 gap-4">
+      <button class="bg-gray-700 p-2 rounded-md" v-bind="previousButtonProps">⬅️ Previous</button>
+      <button class="bg-gray-700 p-2 rounded-md" v-bind="nextButtonProps">Next ➡️</button>
+    </div>
   </div>
 </template>
