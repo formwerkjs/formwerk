ea55c3520cbff3dc0152c0855f71eec2d397383e
Abdelrahman Awad <me@awad.dev>
2025-04-30 01:33:05 +0300

    feat: added a way to navigate between steps

--- packages/core/src/useFormWizard/types.ts
+++ packages/core/src/useFormWizard/types.ts
@@ -1,13 +1,14 @@
-import { InjectionKey } from 'vue';
+import { InjectionKey, MaybeRefOrGetter } from 'vue';
 import { GenericFormSchema } from '../types';
 
 export interface FormWizardStepProps<TSchema extends GenericFormSchema> {
+  id?: string | number;
   schema?: TSchema;
 }
 
 export interface FormWizardContext {
   isStepActive: (stepId: string) => boolean;
-  registerStep: (stepId: string) => void;
+  registerStep: (staticId: string, userStepId: MaybeRefOrGetter<string | number | undefined>) => void;
 }
 
 export const FormWizardContextKey: InjectionKey<FormWizardContext> = Symbol('FormWizardContext');
