66048d33b47e0a43495bd39c9aaed03f4bc4e9d4
Abdelrahman Awad <me@awad.dev>
2025-08-21 11:40:38 +0300

    feat: added validateOn prop to text control

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import FormField from './components/FormField.vue';
 import TextControl from './components/TextControl.vue';
+
+const validationOn = ref(['test']);
 </script>
 
 <template>
   <FormField label="Name">
-    <TextControl required />
+    <TextControl required :validate-on="validationOn" />
+
+    <button @click="validationOn.push('blur')">Add blur</button>
+    {{ validationOn }}
   </FormField>
 </template>
 
