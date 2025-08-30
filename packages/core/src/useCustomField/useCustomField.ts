4b0ba5b299f002346cd18aa11933fe01bbe6d8d5
Abdelrahman Awad <me@awad.dev>
2025-08-31 01:06:39 +0300

    refactor: streamline accessibility props handling in useCustomField

--- packages/core/src/useCustomField/useCustomField.ts
+++ packages/core/src/useCustomField/useCustomField.ts
@@ -3,7 +3,6 @@ import { FieldTypePrefixes } from '../constants';
 import { Reactivify, StandardSchema } from '../types';
 import { exposeField, useFormField } from '../useFormField';
 import { normalizeProps, propsToValues, useUniqId, useCaptureProps } from '../utils/common';
-import { useLabel, useErrorMessage, useDescription } from '../a11y';
 import { useInputValidity } from '../validation';
 import { registerField } from '@formwerk/devtools';
 
@@ -62,23 +61,12 @@ export function useCustomField<TValue = unknown>(_props: Reactivify<CustomFieldP
     schema: props.schema,
   });
 
-  const { errorMessage, isDisabled } = field;
+  const { labelledByProps, accessibleErrorProps, describedByProps, isDisabled } = field;
   const { updateValidity } = useInputValidity({ field });
 
-  const { labelProps, labelledByProps } = useLabel({
-    for: controlId,
-    label: props.label,
-    targetRef: controlEl,
-  });
-
-  const { descriptionProps, describedByProps } = useDescription({
-    inputId: controlId,
-    description: props.description,
-  });
-
-  const { accessibleErrorProps, errorMessageProps } = useErrorMessage({
-    inputId: controlId,
-    errorMessage,
+  field.registerControl({
+    getControlElement: () => controlEl.value,
+    getControlId: () => controlId,
   });
 
   const controlProps = useCaptureProps(() => {
@@ -100,17 +88,10 @@ export function useCustomField<TValue = unknown>(_props: Reactivify<CustomFieldP
   return exposeField(
     {
       /**
-       * Props for the label element.
+       * The id of the control element.
        */
-      labelProps,
-      /**
-       * Props for the description element.
-       */
-      descriptionProps,
-      /**
-       * Props for the error message element.
-       */
-      errorMessageProps,
+      controlId,
+
       /**
        * Props for the control element/group.
        */
